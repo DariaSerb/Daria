fd4e8ca126c99fd04a84a86a287e827055cf93ea
Daria <dariya.serb@gmail.com>
2016-08-10 12:00:01 +0200

    Update Validation.m

--- A/project_2D/Validation.m
+++ A/project_2D/Validation.m
@@ -1,8 +1,10 @@
 function out = Validation(P)
       
-  if (P.radiusout < 0.5*P.lx)&&(P.radiusout < 0.5*P.ly)   
+  if (P.radiusout < 0.5 * P.lx)&&(P.radiusout < 0.5 * P.ly)   
   out = true;
+  disp('correct initial parameters');
   else
-  out = false;    
+  out = false;
+  disp('uncorrect initial parameters'); 
   end
 end

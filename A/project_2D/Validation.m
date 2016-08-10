958bfd489339bd36a42a5b9b6eda578acc4c443e
Daria <dariya.serb@gmail.com>
2016-08-10 11:19:45 +0200

    Update Validation.m

--- A/project_2D/Validation.m
+++ A/project_2D/Validation.m
@@ -1,17 +1,8 @@
-classdef Validation
-    % validation of the initial parameters
+function out = Validation(P)
       
-    properties
-      P = Initialize_Parameters_2D();
-    end
-    
-    methods (Static)
-      function out = ValidMethod()
-        if (P.radiusout < 0.5*P.lx)&&(P.radiusout < 0.5*P.ly)   
-        out = true;
-        else
-        out = false;    
-        end
-      end
-    end
+  if (P.radiusout < 0.5*P.lx)&&(P.radiusout < 0.5*P.ly)   
+  out = true;
+  else
+  out = false;    
+  end
 end

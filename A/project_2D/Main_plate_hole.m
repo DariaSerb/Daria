f15a8e8d41e26e7d7bbe264a6873a55070c699ce
Daria <dariya.serb@gmail.com>
2016-09-29 08:46:41 +0200

    Update Main_plate_hole.m

--- A/project_2D/Main_plate_hole.m
+++ A/project_2D/Main_plate_hole.m
@@ -5,14 +5,12 @@ clc;
 % Begin Timer
 tic
 
+% to create a file with the mesh
 N = 321; W = 0.6; H = 0.4;
 filename = create_name_mesh(N,W,H);
 
-s = strcat(filename,'.msh');
-
-Graphic_display  = 'yes';
-Name_GMSH        = 'C:\Users\Dasha\Documents\MATLAB\myTest_2D\new\meshes_XFEM\s';
-% Name_GMSH        = 'C:\Users\Dasha\Documents\MATLAB\myTest_2D\new\meshes_XFEM\2Dmesh_0_6_0_4_N__321.msh';
+Graphic_display  = 'YES';
+Name_GMSH        = strcat('C:\Users\Dasha\Documents\MATLAB\myTest_2D\new\meshes_XFEM\',filename,'.msh');
 Type_LS          = 'Circle';
 
 % Init parameters in meters
@@ -36,3 +34,19 @@ end
 [Nodes,Elements]  = ReadGMSH(Name_GMSH,Graphic_display);
 
 [freqNum, uNum] = EigenSolutionNum_XFEM(Nodes,Elements,0.07);
+
+for n = 1:N_dr
+   radius(n) = r0 + dr(n);   
+   [freqNum, uNum] = EigenSolutionNum_XFEM(Nodes,Elements,Type_LS,radius(n));
+   freqNum(n,:) = freqNum;
+end
+
+for n = 1:ModeEst
+   figure(n + 1);
+   plot(radius, freqNum(:,n), radius, freqEst(:,n), 'LineWidth', 3);
+   legend('freqNum','freqEst',3)
+   grid on;
+   title(strcat('Natural frequency # ', num2str(n), ' for the position of the initial radius', ' r_0 = ',  num2str(r0), ' [m]'),'FontSize', 12);
+   xlabel('r_0 [m]','FontSize', 12);
+   ylabel('Natural frequency, f [Hz]','FontSize', 12);
+end

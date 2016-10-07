aaf6ac8870f68cd9a70581a1549bcc5588b2ac38
Daria <dariya.serb@gmail.com>
2016-10-07 18:08:31 +0200

    Update test.m

--- A/project_2D/test.m
+++ A/project_2D/test.m
@@ -6,24 +6,81 @@ clc;
 tic
 
 Graphic_display  = 'yes';
-% Name_GMSH     = 'plate_hole.msh';
-% Name_GMSH     = 'plate_hole_190216.msh';
-% Name_GMSH     = 'plate_hole_250416.msh';
-% Name_GMSH     = 'plate_hole_270416.msh';
-% Name_GMSH     = 'plate_hole_270416_2.msh';
-% Name_GMSH     = 'plate_hole_060516.msh';
-% Name_GMSH     = 'plate_hole_140616.msh';
-% Name_GMSH     = '2Dmesh_hole_0_4_0_2_N_293_r_0_040.msh';
-  Name_GMSH     = 'C:\Users\Dasha\Documents\MATLAB\myTest_2D\new\meshes_XFEM\2Dmesh_0_4_0_2_N_258.msh';
-% Name_GMSH     = 'C:\Users\Dasha\Documents\MATLAB\myTest_2D\new\meshes_FEM\2Dmesh_hole_0_4_0_2_N_907_r_0_070.msh';
-% Name_GMSH     = '2Dmesh_hole_0_4_0_2_N_279_r_0_045.msh';
-% Name_GMSH     = '2Dmesh_hole_0_4_0_2_N_261_r_0_050.msh';
+Name_GMSH     = '2Dmesh_0_6_0_4_N__321.msh';
+% Name_GMSH   = '2Dmesh_N_1211_W_0.6_H_0.4.msh'; 	
+
 Type_LS          = 'Circle';
-
 P                = Initialize_Parameters_2D();
-ReferenceElement = SetReferenceElement();
 
-out = Validation(P);
-if out == 0
-return
+% Read of GMSH information
+[Nodes,Elements]  = ReadGMSH(Name_GMSH,Graphic_display);
+% Initialization and computing Level Set
+[Nodes] = ComputeLS(Nodes,Type_LS,P.Data_LS,Graphic_display);
+
+% Separation the elements in three sets
+if strcmp(Type_LS, 'Circle')
+    [Elements] = SeparateElements(Elements,Nodes,Graphic_display);
+
+    % Separation the elements inside the void
+    ind_In  = 1;
+    for ii = 1:size(Elements,1)
+        if Elements(ii,5) == -1;
+            Elems_In(ind_In) = ii;
+            ind_In = ind_In + 1;
+        end
+    end
+else
+    Elements(:,5) = zeros(size(Elements,1),1);
+end
+
+% the definition of displacement of the cut element
+for in=1:size(Elements,1)
+ if Elements(in,5) == 1
+ 
+    Te     = Elements(in,2:4);
+    Xe     = Nodes(Te,2:3);
+    LSe    = Nodes(Te,4); 
+    [p1 p2] = FindPointLS0test(Xe,LSe);
+    node0 = find(LSe < 0);  
+    if length(node0) == 1
+            node1 = node0+1; if(node1) == 4, node1 = 1; end
+            node2 = node0-1; if(node2) == 0, node2 = 3; end
+            P0tr = Te(node0);  
+            P1tr = Te(node1);  
+            P2tr = Te(node2); 
+              if p1new(1,:)> p2new(1,:) && node0 == 2   
+                 tab(1,1:2) = [p1(1,:) p1(2,:)];
+                 tab(2,1:2) = [p2(1,:) p2(2,:)];
+                 tab(3,1:2) = [Nodes(P1tr,2) Nodes(P1tr,3)];
+                 tab(4,1:2) = [Nodes(P2tr,2) Nodes(P2tr,3)];
+                 tab(5,1:2) = [p1(1,:) p1(2,:)];
+                 plot(tab(1:5,1),tab(1:5,2),'c.-') 
+              else 
+                 tab(1,1:2) = [p1(1,:) p1(2,:)];
+                 tab(2,1:2) = [Nodes(P1tr,2) Nodes(P1tr,3)];
+                 tab(3,1:2) = [Nodes(P2tr,2) Nodes(P2tr,3)];
+                 tab(4,1:2) = [p2(1,:) p2(2,:)];
+                 tab(5,1:2) = [p1(1,:) p1(2,:)];
+                 plot(tab(1:5,1),tab(1:5,2),'c.-') 
+              end
+      elseif length(node0) == 2 
+            node1 = setdiff([1 2 3],node0);
+            P0tr = Te(node1); 
+            P1tr = Te(node0(1)); 
+            P2tr = Te(node0(2));
+            if p1(1,:)> p2(1,:)            
+                tab(1,1:2) = [p2(1,:) p2(2,:)];
+                tab(2,1:2) = [Nodes(P0tr,2) Nodes(P0tr,3)];
+                tab(3,1:2) = [p1(1,:) p1(2,:)];
+                tab(4,1:2) = [p2(1,:) p2(2,:)];
+                plot(tab(1:4,1),tab(1:4,2),'c.-') 
+            else
+                tab(1,1:2) = [p1(1,:) p1(2,:)];
+                tab(2,1:2) = [Nodes(P0tr,2) Nodes(P0tr,3)];
+                tab(3,1:2) = [p2(1,:) p2(2,:)];
+                tab(4,1:2) = [p1(1,:) p1(2,:)];
+                plot(tab(1:4,1),tab(1:4,2),'c.-') 
+            end
+    end
+ end
 end

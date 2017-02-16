74d9fb353ab5a08ed6e540f6e794d7f245d28c66
Daria <dariya.serb@gmail.com>
2017-02-16 11:37:10 +0100

    Update Main_plate_hole.m

--- A/project_2D/Main_plate_hole.m
+++ A/project_2D/Main_plate_hole.m
@@ -1,15 +1,6 @@
 clear all;
 close all;
 clc;
-    disp('****************************************************************************************************'); 
-    disp('Calculation of eigenvalue problem in 2D using XFEM and DD for plate with hole /modelling hole by LS/')
-    disp('The transformation between RC and CC is determined by different choice of q(X)/init parameters in m/') 
-    disp('the mesh which used is from GMSH')
-    disp('****************************************************************************************************'); 
-       
-% Calculation of eigenvalue problem in 2D using XFEM for plate with hole /modelling hole by LS/
-% The transformation between RC and CC is determined by different choice of q(X)
-% Integration of [K],[Kdq],[M] and [Mdq] by gauss points using the modification of gauss points for cut elements
 % 16/02/2017
 % Begin Timer
 tic
@@ -58,41 +49,11 @@ end
 % Read of GMSH information
   [Nodes,Elements] = ReadGMSH(Name_GMSH,Graphic_display);
 % [Nodes,Elements] = ReadINP(Name_INP,Graphic_display);
-
 % *************************************************************************
-% test experiment for error estimator
-% load('NodesCurrent.mat','NodesCurrent') 
-% load('ElementsCurrent.mat','ElementsCurrent') 
-% plot_mesh(NodesCurrent,ElementsCurrent)
-% freqEstNew = EigenValueEst_CurrentConf(NodesCurrent,ElementsCurrent);
   error_indicator(Nodes,Elements,Type_LS) 
-% *************************************************************************
-
 % *************************************************************************
 % The calculation of estimated frequencies using DD
   [freqEst,LambdaEst] = EigenValueEst_XFEM_DD(Nodes,Elements,Type_LS,Graphic_display);
-%   [uEst,NodesCurrent,ElementsCurrent,~,~,~,~] = EigenFunctionEst_XFEM_DD(Nodes,Elements,Type_LS,0.0010);
-%   plot_mesh(NodesCurrent,ElementsCurrent)
-%   calc_mesh_quality(NodesCurrent,ElementsCurrent)   
-% save('NodesCurrent.mat','NodesCurrent') 
-% save('ElementsCurrent.mat','ElementsCurrent') 
-  
-% *************************************************************************
-
-% *************************************************************************
-% Calculation of eigenvalue problem numerically (using XFEM) 
-% [freqNumCur,uNumCur] = EigenSolutionNum_XFEM(Nodes,Elements,Type_LS,0.0400);
-% [freqNumCur,uNumCur] = eig_val(NodesCurrent,ElementsCurrent);
-% [freqNum,uNum] = EigenSolutionNum_XFEM(Nodes,Elements,Type_LS,0.0933);
-% [freqNum,uNum] = EigenSolutionNum_XFEM(NodesCurrent,Elements,Type_LS,0.0410);
-% save('freqNum.mat','freqNum')
-% load('NodesCurrent.mat','NodesCurrent') 
-% load('ElementsCurrent.mat','Elements') 
-% plot_mesh(NodesCurrent,ElementsCurrent)
-% [freqNumCur,uNumCur] = eig_val(NodesCurrent,ElementsCurrent);
-% *************************************************************************
-
-% [errorL2,errorH1,nelements]=error_convergence(Nodes,Elements,uNumCur,Type_LS);     
 
 flag = 0;
 if flag == 1
@@ -102,13 +63,11 @@ freqXFEM = zeros(length(dr),ModeEst);
    for n = 1:N_dr
       radius(n) = r0 + dr(n);   
       [freqNum,uNum] = EigenSolutionNum_XFEM(Nodes,Elements,Type_LS,radius(n));
-%     [freqNum,uNum] = EigenSolutionNum_XFEM(NodesCurrent,ElementsCurrent,Type_LS,radius(n));
       freqXFEM(n,:)  = freqNum;
    end
 end
 
 plot_freq(radius,freqXFEM,freqEst);
-plot_MAC(Nodes,Elements,Type_LS);
 
 % End Timer
 computation_time = toc;

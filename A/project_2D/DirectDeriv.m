0b32a35d0971f3894cdebd0c42bfab5d3f643e8c
Daria <dariya.serb@gmail.com>
2017-02-16 11:53:12 +0100

    Update DirectDeriv.m

--- A/project_2D/DirectDeriv.m
+++ A/project_2D/DirectDeriv.m
@@ -15,19 +15,7 @@ for n = 1:ModeEst
     var = (stiffdq + stiffdq - stiffdivq + lambda(n) * massdivq) * V(:,n);
     numer = (V(:,n))' * var;
     DLambda(1,n) = - numer/mui(n);
-   
-    % the calculation of DD of the second order
-      if iorder == 2;
-      term1 = 2 * DLambda(1,n) * massdivq;    
-      sum   = (1/2) * (- stiff + lambda(n) * mass) + 2 * stiff;
-      term2 = 2 * sum ;
-      term3 = stiffdq + term4;
-      var2 = (term1 + term2 - term3) * V(:,n); 
-      numer2 = (V(:,n))' * var2;
-      D2Lambda(1,n) = - numer2/mui(n);
-      end
-    
-    for m = 1:ModeCnt
+        for m = 1:ModeCnt
         if n == m
             continue;
         end

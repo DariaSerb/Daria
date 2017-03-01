d58963939db0fbcd67f1e679296de8c4db846a98
Daria <dariya.serb@gmail.com>
2017-03-01 08:06:26 +0100

    Update change_configuration.m

--- A/project_2D/change_configuration.m
+++ A/project_2D/change_configuration.m
@@ -6,8 +6,7 @@ NodCur      = zeros(size(Nodes,1),3);
 NodCur(:,1) = (1:length(Nodes));
 
 for in = 1:size(Nodes,1);
-  [~,qm(in,:),~,~] = q_calc_funcMain(Nodes(in,2:3));   
-% [qm(in,:),~, ~]  = q_calc_funcPolynom4(Nodes(in,2:3));   
+ [qm(in,:),~, ~]  = q_calc_funcPolynom4(Nodes(in,2:3));   
  
   NodCur(in,2) = Nodes(in,2) + qm(in,1)*dTau;
   NodCur(in,3) = Nodes(in,3) + qm(in,2)*dTau;
